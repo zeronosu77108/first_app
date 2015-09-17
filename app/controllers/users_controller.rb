e58e0112eba80648eaa788d48813c2e61323f4b4
zeronosu77108 <zeronosu77108@docomo.ne.jp>
2015-09-17 23:47:10 +0900

    show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,5 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = Hash.new
-    @user[:name] = 'Shohei Aoki'
-    @user[:username] = 'moyahima'
-    @user[:location] = 'Tottori, Japan'
-    @user[:about] = 'Nice to meet you.'
   end
 end
